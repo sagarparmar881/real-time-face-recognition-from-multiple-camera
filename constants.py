a5a51584f962314dd83c391541eab7fe4e93ff16
Sagar Parmar <47896458+sagarparmar881@users.noreply.github.com>
2023-05-20 12:36:17 +0530

    Updated constants.py

--- constants.py
+++ constants.py
@@ -1,3 +1,2 @@
-predictor_path = "data/data_dlib/shape_predictor_68_face_landmarks.dat"
+predictor_file_path = "data/data_dlib/shape_predictor_68_face_landmarks.dat"
 face_reco_model_file_path = "data/data_dlib/dlib_face_recognition_resnet_model_v1.dat"
-path=""
