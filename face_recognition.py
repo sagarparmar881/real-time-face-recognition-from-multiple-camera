dbfeec6813d8d6f83bbbffb02989216b263d2578
Sagar Parmar <47896458+sagarparmar881@users.noreply.github.com>
2023-05-20 11:43:53 +0530

    Initial Commit

---
+++ face_recognition.py
@@ -0,0 +1,15 @@
+import cv2
+import threading
+import dlib
+import numpy as np
+import cv2
+import os
+import pandas as pd
+import time
+import logging
+import threading
+import _thread as thread
+import cv2
+import urllib.request
+import numpy as np
+import time
